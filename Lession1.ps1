0e2107d73f3a1c9e4b205a0cdd7c21df2d9891bb
ZrvinS <zrvinsunar@gmail.com>
2024-05-02 22:34:49 +0530

    datatype

---
+++ Lession1.ps1
@@ -0,0 +1,24 @@
+
+#Powerhsell is base on .NET
+# Variables: is a container, that holds data 
+$cup1 = "Milk"
+$cup2 = "Coffee"
+
+$cup3 = $cup1 + $cup2 
+$cup3
+
+#DataTypes
+
+# Int, float, bool, string, double, int32, int64, int16
+
+$number = 2354
+$number.GetType() #Int will only store numbers
+
+$number = "hello world"
+$number.GetType() #string will only store string value like characters
+
+$number = 34543.56544
+$number.GetType() #Bool store only true or fals
+
+$number = $true
+$number.GetType() #float will store 23.653 value
