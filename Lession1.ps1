3ada61fcff325202a048e598a6219d5b5e63ca84
ZrvinS <zrvinsunar@gmail.com>
2024-05-02 22:43:52 +0530

    update2

--- Lession1.ps1
+++ Lession1.ps1
@@ -1,6 +1,8 @@
 
 #Powerhsell is base on .NET
 # Variables: is a container, that holds data 
+
+#$ is put in front of any word to declare it as a variable or when we call the value of the variable
 $cup1 = "Milk"
 $cup2 = "Coffee"
 
@@ -11,14 +13,15 @@ $cup3
 
 # Int, float, bool, string, double, int32, int64, int16
 
-$number = 2354
+$number = 23543546346
 $number.GetType() #Int will only store numbers
 
 $number = "hello world"
 $number.GetType() #string will only store string value like characters
 
 $number = 34543.56544
-$number.GetType() #Bool store only true or fals
+$number.GetType() #float/double store only true or fals
 
 $number = $true
 $number.GetType() #float will store 23.653 value
+
